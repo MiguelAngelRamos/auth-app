4fc327e02dd7443cd4d6c9a59b7c6bae8673daf2
Miguel Angel Ramos <return.infox@gmail.com>
2024-04-23 20:34:44 -0400

    formulario de registro y rutas

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import register
-
+#* http://127.0.0.1:8080/register
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/'),
